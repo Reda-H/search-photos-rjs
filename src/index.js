7f50a291d3f509fe927d7cb33a7d0319afdf05bf
Reda-H <herradi.r@gmail.com>
2021-12-13 12:41:45 +0100

    dummy commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApYevI4m-gyhZ7IKXynLmnmmNGYr6_5vQ",
+  authDomain: "searchphotos-7eb6a.firebaseapp.com",
+  projectId: "searchphotos-7eb6a",
+  storageBucket: "searchphotos-7eb6a.appspot.com",
+  messagingSenderId: "51593743579",
+  appId: "1:51593743579:web:2593b80753ddc7d4d990fe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
